911b6ded9b0630996814e19225c1b4612ce3e5d3
Farizz <farisabdillah1806@gmail.com>
2025-11-19 19:34:38 +0700

    Remove : Unnecesarry Files

--- public/burger.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.burger');
-const sidebarc = document.querySelector('.sidebarc');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  sidebarc.classList.toggle('active');
-})
